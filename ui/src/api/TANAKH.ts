74b450b5154670c4e9e64476cc0034707ab4fb7a
David Finson <david@sanda.dev>
2020-11-10 12:07:30 +0200

    added hardcoded hebrew book names to Tanakh.ts

--- ui/src/api/TANAKH.ts
+++ ui/src/api/TANAKH.ts
@@ -45,7 +45,70 @@ export const WRITINGS = {
     ESTHER: 'WRITINGS/Esther'
 }
 
+interface IDictionary {
+    [index: string]: string;
+}
+
+const HEBREW_NAMES_DICT = {
+    'TORAH/Genesis': 'בראשית',
+    'TORAH/Exodus': 'שמות',
+    'TORAH/Leviticus': 'ויקרא',
+    'TORAH/Numbers': 'במדבר',
+    'TORAH/Deuteronomy': 'דברים',
+
+    'PROPHETS/Joshua': 'יהושוע',
+    'PROPHETS/Judges 1': 'שופטים א',
+    'PROPHETS/Judges 2': 'שופטים ב',
+    'PROPHETS/Samuel 1': 'שמואל א',
+    'PROPHETS/Samuel 2': 'שמואל ב',
+    'PROPHETS/Kings 1': 'מלכים א',
+    'PROPHETS/Kings 2': 'מלכים ב',
+    'PROPHETS/Isaiah': 'ישעיה',
+    'PROPHETS/Jeremiah': 'ירמיהו',
+    'PROPHETS/Ezekiel': 'יחזקאל',
+    'PROPHETS/Hosea': 'הושע',
+    'PROPHETS/Joel': 'יואל',
+    'PROPHETS/Amos': 'עמוס',
+    'PROPHETS/Obadiah': 'עובדיה',
+    'PROPHETS/Jonah': 'יונה',
+    'PROPHETS/Micha': 'מיכה',
+    'PROPHETS/Nahum': 'נחום',
+    'PROPHETS/Habakkuk': 'חבקוק',
+    'PROPHETS/Zephania': 'צפניה',
+    'PROPHETS/Haggai': 'חגי',
+    'PROPHETS/Zechariah': 'זכריה',
+    'PROPHETS/Malachi': 'מלכי',
+    'PROPHETS/Daniel': 'דניאל',
+    'PROPHETS/Ezra': 'עזרא',
+    'PROPHETS/Nehemiah': 'נחמיה',
+    'PROPHETS/Chronicles': 'דברי הימים',
+
+    'WRITINGS/Psalms': 'תהלים',
+    'WRITINGS/Proverbs': 'משלי',
+    'WRITINGS/Job': 'איוב',
+    'WRITINGS/Ruth': 'רות',
+    'WRITINGS/Lamentations': 'איכה',
+    'WRITINGS/Ecclesiastes': 'קוהלת',
+    'WRITINGS/Esther': 'אסתר'
+} as IDictionary;
+
 export function stripPrefix(pathWithPrefix: string): string{
     return pathWithPrefix.split("/")[1];
+}
+
+export function toHebrewBookName(path: string): string{
+    return HEBREW_NAMES_DICT[path];
+}
+
+export function hebrewBooksInTorah(){
+    return Object.keys(TORAH).map(bookName => toHebrewBookName(HEBREW_NAMES_DICT[bookName]))
+}
+
+export function hebrewBooksInProphets(){
+    return Object.keys(PROPHETS).map(bookName => toHebrewBookName(HEBREW_NAMES_DICT[bookName]))
+}
+
+export function hebrewBooksInWritings(){
+    return Object.keys(WRITINGS).map(bookName => toHebrewBookName(HEBREW_NAMES_DICT[bookName]))
 }
 
